fbf38e440baf3004d84f0a69b8a64b23ce5061a7
Wasim Hyder <wasim@creativefaze.com>
2023-07-27 01:08:42 +0500

    Added input form

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 const MealItem = props => {
   const price = `$${props.price.toFixed(2)}`
   return <li className={classes.meal}>
@@ -8,7 +9,7 @@ const MealItem = props => {
       <div className={classes.price}>{price}</div>
     </div>
     <div>
-
+      <MealItemForm />
     </div>
   </li>
 };
